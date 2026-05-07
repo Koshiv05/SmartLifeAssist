e2360e0b8e34e8420b9f5992bc585e40db2ac67e
Koshiv Sharma <koshiv2001@gmail.com>
2026-05-07 14:11:45 +1000

    Added Firestore database export to Firebase service

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,15 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZFT94QgSp9evAlCQIgm2AdYnzu7Spq9Y",
-  authDomain: "smartlifeassist.firebaseapp.com",
-  projectId: "smartlifeassist",
-  storageBucket: "smartlifeassist.firebasestorage.app",
-  messagingSenderId: "909913080225",
-  appId: "1:909913080225:web:161d21eb41f001d40ccbe0",
+    apiKey: "AIzaSyCZFT94QgSp9evAlCQIgm2AdYnzu7Spq9Y",
+    authDomain: "smartlifeassist.firebaseapp.com",
+    projectId: "smartlifeassist",
+    storageBucket: "smartlifeassist.firebasestorage.app",
+    messagingSenderId: "909913080225",
+    appId: "1:909913080225:web:161d21eb41f001d40ccbe0",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
