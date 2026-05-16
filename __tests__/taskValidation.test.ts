40a696e445ab144d9a17742360a1ff3abc9d71c3
Koshiv Sharma <koshiv2001@gmail.com>
2026-05-16 16:03:30 +1000

    Added Jest task validation tests

---
+++ __tests__/taskValidation.test.ts
@@ -0,0 +1,21 @@
+describe('Task Validation', () => {
+  test('Task title should not be empty', () => {
+    const title = 'Buy groceries';
+
+    expect(title.length).toBeGreaterThan(0);
+  });
+
+  test('Due date should exist', () => {
+    const dueDate = '20/05/2026';
+
+    expect(dueDate).toBeTruthy();
+  });
+
+  test('Reminder type should be valid', () => {
+    const reminderType = 'time';
+
+    expect(['time', 'location', '']).toContain(
+      reminderType
+    );
+  });
+});
